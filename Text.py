6079ef48f5ec5bcb6638d14ef3e9c78d72c85c41
harshbo <124868729+harshbo@users.noreply.github.com>
2023-02-15 20:45:22 +0530

    Update Text.py

--- Text.py
+++ Text.py
@@ -5,6 +5,6 @@ def submit():
 window = Tk()
 text = Text(window,bg="light yellow",font =("Ink Free",20),height=10,width=20,fg = "brown")
 text.pack()
-button = Button(window,text="submit",command=submit,cursor ="trek")
+button = Button(window,text="Click Me",command=submit,cursor ="trek")
 button.pack()
 window.mainloop()
