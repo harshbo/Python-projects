52e43cd4c04214dc0c19318a192a82f7d18aaee4
harshbo <124868729+harshbo@users.noreply.github.com>
2023-02-15 18:56:24 +0530

    Update Password.py

--- Password.py
+++ Password.py
@@ -4,6 +4,6 @@ u = l.upper();
 n ="1234567890"
 s = "!@#$%^&*()_+=-}{\/?:;[]<>,."
 all = l+u+n+s
-length = 5
+length = 8
 password = "".join(random.sample(all,length))
 print("Suggested Password : ", password);
