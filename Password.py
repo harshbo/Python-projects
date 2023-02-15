a260203d890b955349fb055db5874a66189da7f2
harshbo <124868729+harshbo@users.noreply.github.com>
2023-02-15 18:39:12 +0530

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,9 @@
+import random
+l = "abcdefghijklmnopqrstuvwxyz"
+u = l.upper();
+n ="1234567890"
+s = "!@#$%^&*()_+=-}{\/?:;[]<>,."
+all = l+u+n+s
+length = 5
+password = "".join(random.sample(all,length))
+print("Suggested Password : ", password);
